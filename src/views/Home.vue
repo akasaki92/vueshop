96dab0894b1dddc03165ed93d3b720e59ad456a3
Adam Santiaji <adam.wikrama@gmail.com>
2020-11-30 11:21:37 +0700

    change Home views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,14 +34,7 @@
             </div>
             <v-layout wrap>
                 <v-flex v-for="book in books" :key="`book-` + book.id" xs6>
-                    <v-card :to="'/book/' + book.slug">
-                        <v-img :src="getImage('/books/'+book.cover)" class="white--text">
-                            <v-card-title
-                                class="fill-height align-end"
-                                v-text="book.title"
-                            ></v-card-title>
-                        </v-img>
-                    </v-card>
+                    <book-item :book="book" />
                 </v-flex>
             </v-layout>
         </v-container>
@@ -54,6 +47,12 @@
 import '../plugins/axios'
 
 export default {
+    components: { 
+        BookItem:() => import(
+            /* webpackChunkName: "book-item" */
+            '@/components/BookItem.vue'
+        ) 
+    },
     name: "Home",
     data: () => ({
         categories: [],
