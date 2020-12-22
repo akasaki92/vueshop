30f9900f0a318e39e7107932200961bafecc99fd
Adam Santiaji <adam.wikrama@gmail.com>
2020-12-22 11:45:00 +0700

    add payment views

--- src/router/index.js
+++ src/router/index.js
@@ -50,15 +50,18 @@ const routes = [
         meta: { auth: true }
     },
     {
+        path: '/payment',
+        name: 'payment',
+        component: () => import( /* webpackChunkName: payment */ '../views/Payment.vue'),
+        meta: { auth: true }
+    },
+    {
         path: '/about',
         name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(
-            /* webpackChunkName: "about" */
-            '../views/About.vue'
-        )
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
 ]
 
